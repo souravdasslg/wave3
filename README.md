ad5d0293e87b822dc5c38c55a1deabfc1311c204
Sourav Das <souravdasslg95@gmail.com>
2023-02-12 14:20:01 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+👋3 [Wave3]
+My First Web3 App
---
+++ contracts/WavePortal.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.17;
+import "hardhat/console.sol";
+
+contract WavePortal {
+    constructor() {
+        console.log("I am a contract and I am smart");
+    }
+}
